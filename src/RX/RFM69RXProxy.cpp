95df1e5d595a21a28171a6b91930e400a83c164b
Joshua Gall <josh@imtraum.com>
2018-11-08 18:34:58 -0600

    refactored the objects in the RX namespace

--- src/RX/RFM69RXProxy.cpp
+++ src/RX/RFM69RXProxy.cpp
@@ -4,45 +4,26 @@
 #include "RFM69RXProxy.h"
 
 namespace RX {
-    /************ Radio Setup ***************/
-    // Change to 434.0 or other frequency, must match RX's freq!
-    #define RF69_FREQ 915.0
-
-    // who am i? (server address)
-    #define SERVER_ADDRESS     1
-    
-    #define RFM69_CS      12
-    #define RFM69_RST     11
-    #define RFM69_IRQ     13
-    #define RFM69_IRQN    digitalPinToInterrupt(RFM69_IRQ)
-    //#define LED           13
-
-    // Singleton instance of the radio driver
     // TODO: What happens if we pass the RFM69_IRQN value rather than the RFM69_IRQ value to the below function?
     // would this mean we wouldn't have to re-initialize and/or shut down the wifi every time we hit our loop in
     // the sketch?!
-    RH_RF69 rf69(RFM69_CS, RFM69_IRQ);
-
-    // Class to manage message delivery and receipt, using the driver declared above
-    RHReliableDatagram manager(rf69, SERVER_ADDRESS);
-
-    // Constructor
-    RFM69RXProxy::RFM69RXProxy() { }
+    RFM69RXProxy::RFM69RXProxy() :
+        rf69(this->RFM69_CS, this->RFM69_IRQ),
+        manager(this->rf69, this->SERVER_ADDRESS)
+    { }
 
     void RFM69RXProxy::Initialize() {
         //pinMode(LED, OUTPUT);     
-        pinMode(RFM69_RST, OUTPUT);
-        digitalWrite(RFM69_RST, LOW);
-
-        Serial.println("Feather Addressed RFM69 RX Test!");
+        pinMode(this->RFM69_RST, OUTPUT);
+        digitalWrite(this->RFM69_RST, LOW);
 
         // manual reset
-        digitalWrite(RFM69_RST, HIGH);
+        digitalWrite(this->RFM69_RST, HIGH);
         delay(10);
-        digitalWrite(RFM69_RST, LOW);
+        digitalWrite(this->RFM69_RST, LOW);
         delay(10);
         
-        if (!manager.init()) {
+        if (!this->manager.init()) {
             Serial.println("RFM69 radio init failed");
             while (1);
         }
@@ -50,53 +31,53 @@ namespace RX {
         Serial.println("RFM69 radio init OK!");
         // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
         // No encryption
-        if (!rf69.setFrequency(RF69_FREQ)) {
+        if (!this->rf69.setFrequency(this->RF69_FREQ)) {
             Serial.println("setFrequency failed");
         }
 
         // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
         // ishighpowermodule flag set like this:
-        rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+        this->rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
 
         // The encryption key has to be the same as the one in the server
         uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                             0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-        rf69.setEncryptionKey(key);
+        this->rf69.setEncryptionKey(key);
         
         //pinMode(LED, OUTPUT);
 
-        Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+        Serial.print("RFM69 radio @");  Serial.print((int)this->RF69_FREQ);  Serial.println(" MHz");
     }
 
-    // Dont put these on the stack, they are re-used; this is more efficient.
-    uint8_t acknowledgeData[] = "acknowledged from node 1";
-    uint8_t messageBuffer[RH_RF69_MAX_MESSAGE_LEN];
+    // NOTE: for some reason, that I don't understand exactly, this needs to sit here rather than with the class.
+    // dumb on me.
+    uint8_t acknowledgeData[25] = "acknowledged from node 1";    
 
     SensorTransmissionResult RFM69RXProxy::Listen() {
         SensorTransmissionResult result;
         result.HasResult = false;
 
-        if (manager.available()) {
+        if (this->manager.available()) {
             // Wait for a message addressed to us from the client
-            uint8_t messageBufferLength = sizeof(messageBuffer);
+            uint8_t messageBufferLength = sizeof(this->messageBuffer);
             uint8_t from;
 
-            if (manager.recvfromAck(messageBuffer, &messageBufferLength, &from)) {
-                messageBuffer[messageBufferLength] = 0; // zero out (I don't really know why we do this to be honest)
+            if (this->manager.recvfromAck(this->messageBuffer, &messageBufferLength, &from)) {
+                this->messageBuffer[messageBufferLength] = 0; // zero out (I don't really know why we do this to be honest)
 
                 // deserialize the message sent into our common data structure
                 SensorData messageData;
-                memcpy(&messageData, messageBuffer, sizeof(messageData));
+                memcpy(&messageData, this->messageBuffer, sizeof(messageData));
                 result.Data = messageData;
                 result.TransmissionSource = from;
-                result.RSSI = rf69.lastRssi();
+                result.RSSI = this->rf69.lastRssi();
                 result.HasResult = true;
                 
                 //blink LED 3 times, 40ms between blinks
                 //Blink(LED, 40, 3);
 
                 // Send a reply back to the originator client
-                if (!manager.sendtoWait(acknowledgeData, sizeof(acknowledgeData), from)) {
+                if (!this->manager.sendtoWait(acknowledgeData, sizeof(acknowledgeData), from)) {
                     Serial.println("Sending failed (no ack)");
                 }
             }
@@ -104,13 +85,4 @@ namespace RX {
 
         return result;
     }
-    
-    // void RFM69RXProxy::Blink(byte PIN, byte DELAY_MS, byte loops) {
-    //     for (byte i = 0; i < loops; i++)  {
-    //         digitalWrite(PIN,HIGH);
-    //         delay(DELAY_MS);
-    //         digitalWrite(PIN,LOW);
-    //         delay(DELAY_MS);
-    //     }
-    // }
 }
--- src/RX/RFM69RXProxy.h
+++ src/RX/RFM69RXProxy.h
@@ -1,21 +1,31 @@
-#include "..\Sensors\SensorData.h"
-#include "..\SensorTransmissionResult.h"
-
 namespace RX {
     #ifndef RFM69RXProxy_h
         #define RFM69RXProxy_h
 
-        // library interface description
+        #include <SPI.h>
+        #include <RH_RF69.h>
+        #include <RHReliableDatagram.h>
+        #include "..\Sensors\SensorData.h"
+        #include "..\SensorTransmissionResult.h"
+        
         class RFM69RXProxy {
             public:
                 RFM69RXProxy();
 
                 void Initialize();
                 SensorTransmissionResult Listen();
-
-            // library-accessible "private" interface
             private:
-                void Blink(byte PIN, byte DELAY_MS, byte loops);
+                const int16_t RF69_FREQ = 915.0;
+                const byte SERVER_ADDRESS = 1;
+                const byte RFM69_CS = 12;
+                const byte RFM69_RST = 11;
+                const byte RFM69_IRQ = 13;
+                const byte RFM69_IRQN = digitalPinToInterrupt(RFM69_IRQ);
+
+                RH_RF69 rf69;
+                RHReliableDatagram manager;
+
+                uint8_t messageBuffer[RH_RF69_MAX_MESSAGE_LEN];
         };
 
     #endif
