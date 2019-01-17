08f6e23f9ff1cbba00e2300c09b36578f58ec6f9
Joshua Gall <josh@imtraum.com>
2019-01-16 19:45:20 -0600

    added a devices enumeration

---
+++ src/Devices.h
@@ -0,0 +1,14 @@
+#ifndef Devices_h
+#define Devices_h
+
+///<summary>Represents the various devices that can exist as part of the system.</summary>
+enum Devices : uint8_t
+{
+	ClimateController = 1u << 0,
+	ClimateSensorArray = 1u << 1,
+	ParticulateSensorArray = 1u << 2,
+	SystemMonitor = 1u << 3,
+	AllListeners = ClimateController | SystemMonitor
+};
+
+#endif
