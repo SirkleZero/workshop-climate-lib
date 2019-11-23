680561414db46034eb958b846e39a39ba7bc80ce
Joshua Gall <josh@imtraum.com>
2019-11-23 11:16:54 -0600

    working to refactor the display model

---
+++ src/Display/TFTDisplay.cpp
@@ -0,0 +1,476 @@
+#include "TFTDisplay.h"
+
+using namespace Sensors;
+
+namespace Display {
+#pragma region constructor and initialization
+
+	/// <summary>Initializes a new instance of the <see cref="TFTDisplay"/> class.</summary>
+	TFTDisplay::TFTDisplay() :
+		tft(TFT_CS, TFT_DC),
+		ts(STMPE_CS),
+		humidityArea(240, 85, 240, 170, ScreenRegion::Home),
+		temperatureArea(0, 85, 240, 170, ScreenRegion::Home),
+		settingsButton(435, 0, 45, 45, ScreenRegion::Home),
+		homeButton(0, 0, 45, 45, ScreenRegion(ScreenRegion::Humidity | ScreenRegion::Settings | ScreenRegion::Temperature)),
+		centeredMessageBox(0, 160, 480, 10)
+	{}
+
+	/// <summary>Executes initialization logic for the object.</summary>
+	/// <returns>An <see cref="InitializationResult"/> that describes the result of initialization.</returns>
+	InitializationResult TFTDisplay::Initialize()
+	{
+		InitializationResult result;
+
+		tft.begin();
+
+		noInterrupts();
+		tft.setRotation(TFTDisplay::ScreenOrientation);
+		interrupts();
+
+		this->height = tft.height();
+		this->width = tft.width();
+
+		this->touchscreenExists = ts.begin();
+
+		result.IsSuccessful = true;
+		return result;
+	}
+
+#pragma endregion
+
+	/// <summary>Clears all displayed content from the screen by wiping it with a black background.</summary>
+	void TFTDisplay::Clear()
+	{
+		noInterrupts();
+		tft.fillScreen(TFTDisplay::BackgroundColor);
+		interrupts();
+	}
+
+	void TFTDisplay::Display()
+	{
+		// use the Touched() function to determine if a screen area has been pressed, and if so, where.
+		this->Display(this->Touched());
+	}
+
+	void TFTDisplay::Display(ScreenRegion region)
+	{
+		// Depending on which screen region needs to be displayed, update the display.
+		// the call to touched will return ScreenRegion "none" as its default, since if
+		// nothing was clicked on, well, that would make sense right? But, we don't want 
+		// this Display() method to perpetually update the display, so we ignore values
+		// of none, instead only focusing on if something was actually clicked in a
+		// valid area.
+		// We also check that actual data was updated which requires a redraw of the screen.
+		this->selectedRegion = region;
+		this->regionChanged = this->selectedRegion != ScreenRegion::None && this->selectedRegion != this->activeRegion;
+		if (this->DisplayUpdatable())
+		{
+			// nothing was actually clicked on, stay on the view we are currently rendering
+			if (region != ScreenRegion::None)
+			{
+				this->activeRegion = this->selectedRegion;
+			}
+
+			switch (this->activeRegion)
+			{
+				case ScreenRegion::Home:
+					Serial.println(F("ScreenRegion::Home"));
+					this->DisplayHomeScreen();
+					break;
+				case ScreenRegion::Humidity:
+					Serial.println(F("ScreenRegion::Humidity"));
+					this->DisplayHumidityScreen();
+					break;
+				case ScreenRegion::Settings:
+					Serial.println(F("ScreenRegion::Settings"));
+					this->DisplaySettingsScreen();
+					break;
+				case ScreenRegion::StatusMessage:
+					Serial.println(F("ScreenRegion::StatusMessage"));
+					this->DisplayMessageScreen();
+					break;
+				case ScreenRegion::Temperature:
+					Serial.println(F("ScreenRegion::Temperature"));
+					this->DisplayTemperatureScreen();
+					break;
+				default:
+					Serial.println(F("default"));
+					this->DisplayHomeScreen();
+					break;
+			}
+
+			// configure the button areas per the region we've clicked on / screen we are displaying.
+			this->ConfigureButtons(this->activeRegion);
+
+			// once the display has been updated, reset the state variables that track change status
+			this->DisplayUpdated();
+		}
+	}
+
+	void TFTDisplay::ConfigureButtons(ScreenRegion region)
+	{
+		this->humidityArea.Enable(region);
+		this->temperatureArea.Enable(region);
+		this->settingsButton.Enable(region);
+		this->homeButton.Enable(region);
+	}
+
+	bool TFTDisplay::DisplayUpdatable()
+	{
+		return this->dataChanged || this->regionChanged;
+	}
+
+	void TFTDisplay::DisplayUpdated()
+	{
+		this->dataChanged = false;
+		this->regionChanged = false;
+	}
+
+	/// <summary></summary>
+	ScreenRegion TFTDisplay::Touched()
+	{
+		if (!this->ts.bufferEmpty())
+		{
+			// Convert the point that was clicked to compensate for the screen orientation. While
+			// the display knows it's orientation, the touchscreen does not.
+			TS_Point p = this->ts.getPoint();
+
+			// NOTE: this 3.5" calculation is different than the one for the 2.4" display
+			// not entirely sure why the larger display version behaves differently than the
+			// smaller version.
+			int y = map(p.x, TFTDisplay::TSMaxY, TFTDisplay::TSMinY, 0, tft.height());
+			int x = map(p.y, TFTDisplay::TSMinX, TFTDisplay::TSMaxX, 0, tft.width());
+
+			if (this->humidityArea.Contains(x, y))
+			{
+				return ScreenRegion::Humidity;
+			}
+			if (this->temperatureArea.Contains(x, y))
+			{
+				return ScreenRegion::Temperature;
+			}
+			if (this->settingsButton.Contains(x, y))
+			{
+				return ScreenRegion::Settings;
+			}
+			if (this->homeButton.Contains(x, y))
+			{
+				return ScreenRegion::Home;
+			}
+		}
+
+		return ScreenRegion::None;
+	}
+
+	void TFTDisplay::LoadMessage(const __FlashStringHelper* message)
+	{
+		this->currentMessage = message;
+		this->dataChanged = true;
+	}
+
+	/// <summary>Prints sensor information to the screen.</summary>
+	/// <param name="data">The <see cref="BME280Data"> containing readings from the sensors.</param>
+	void TFTDisplay::LoadSensorData(BME280Data data)
+	{
+		this->currentBME280Data = data;
+		this->dataChanged = true;
+	}
+
+	/// <summary>Draws the initial static elements of the display.</summary>
+	void TFTDisplay::LayoutHomeScreen()
+	{
+		if (this->regionChanged)
+		{
+			Serial.println(F("laying out home screen"));
+			noInterrupts();
+
+			tft.fillScreen(TFTDisplay::BackgroundColor);
+
+			// for centering
+			int16_t centeredTextXPosition;
+
+			// draw our centerline
+			tft.drawFastVLine(240, 30, 260, TFTDisplay::LayoutLineColor);
+
+			// set font
+			tft.setFont(&calibrib12pt7b);
+			tft.setTextColor(TFTDisplay::LayoutTextColor);
+			tft.setTextSize(1);
+
+			// for temperature
+			char* temperatureLabel = "Fahrenheit";
+			//tft.drawRect(temperatureArea.x, temperatureArea.y, temperatureArea.width, temperatureArea.height, TFTDisplay::LayoutLineColor);
+			centeredTextXPosition = GetCenteredPosition(temperatureLabel, temperatureArea.x, temperatureArea.y, temperatureArea.width);
+			tft.setCursor(centeredTextXPosition, temperatureArea.y + temperatureArea.height - 5);
+			tft.println(temperatureLabel);
+
+			// for Humidity
+			char* humidityLabel = "% Humidity";
+			//tft.drawRect(humidityArea.x, humidityArea.y, humidityArea.width, humidityArea.height, TFTDisplay::LayoutLineColor);
+			centeredTextXPosition = GetCenteredPosition(humidityLabel, humidityArea.x, humidityArea.y, humidityArea.width);
+			tft.setCursor(centeredTextXPosition, humidityArea.y + humidityArea.height - 5);
+			tft.println(humidityLabel);
+
+			// draw the box for the settings screen
+			//tft.drawRect(settingsButton.x, settingsButton.y, settingsButton.width, settingsButton.height, TFTDisplay::LayoutLineColor);
+
+			// draw the box for the home screen
+			//tft.drawRect(homeButton.x, homeButton.y, homeButton.width, homeButton.height, TFTDisplay::LayoutLineColor);
+
+			interrupts();
+		}
+	}
+
+	void TFTDisplay::LayoutHumidityScreen()
+	{
+		if (this->regionChanged)
+		{
+			Serial.println(F("laying out humidity screen"));
+
+			noInterrupts();
+
+			tft.fillScreen(TFTDisplay::BackgroundColor);
+
+			tft.drawFastHLine(70, 165, 340, TFTDisplay::LayoutLineColor);
+
+			interrupts();
+		}
+	}
+
+	void TFTDisplay::LayoutTemperatureScreen()
+	{
+		if (this->regionChanged)
+		{
+			Serial.println(F("laying out temperature screen"));
+
+			noInterrupts();
+
+			tft.fillScreen(TFTDisplay::BackgroundColor);
+
+			tft.drawFastHLine(70, 165, 340, TFTDisplay::LayoutLineColor);
+
+			interrupts();
+		}
+	}
+
+	void TFTDisplay::LayoutSettingsScreen()
+	{
+		if (this->regionChanged)
+		{
+			Serial.println(F("laying out settings screen"));
+
+			noInterrupts();
+
+			tft.fillScreen(TFTDisplay::BackgroundColor);
+
+			tft.drawFastHLine(70, 165, 340, TFTDisplay::LayoutLineColor);
+
+			interrupts();
+		}
+	}
+
+	void TFTDisplay::LayoutMessageScreen()
+	{
+		if (this->regionChanged)
+		{
+			Serial.println(F("laying out message screen"));
+
+			noInterrupts();
+
+			tft.fillScreen(TFTDisplay::BackgroundColor);
+
+			tft.drawFastHLine(70, 165, 340, TFTDisplay::LayoutLineColor);
+
+			interrupts();
+		}
+	}
+
+	void TFTDisplay::DisplayHomeScreen()
+	{
+		this->LayoutHomeScreen();
+
+		noInterrupts();
+
+		// NOTE: each of the method calls here first over-write the previous value with the background, and 
+		// then the current value in the defined color for readings.
+		if (this->IntegerPartChanged(this->previousBME280Data.Temperature, this->currentBME280Data.Temperature) || this->regionChanged)
+		{
+			this->PrintTemperature(&this->previousBME280Data, TFTDisplay::BackgroundColor);
+			this->PrintTemperature(&this->currentBME280Data, TFTDisplay::ReadingsTextColor);
+		}
+
+		if (this->IntegerPartChanged(this->previousBME280Data.Humidity, this->currentBME280Data.Humidity) || this->regionChanged)
+		{
+			this->PrintHumidity(&this->previousBME280Data, TFTDisplay::BackgroundColor);
+			this->PrintHumidity(&this->currentBME280Data, TFTDisplay::ReadingsTextColor);
+		}
+
+		interrupts();
+
+		this->previousBME280Data = this->currentBME280Data;
+	}
+
+	void TFTDisplay::DisplayHumidityScreen()
+	{
+		this->LayoutHumidityScreen();
+
+		noInterrupts();
+
+		this->LoadMessage(F("Coming Soon!"));
+		this->PrintMessage();
+
+		interrupts();
+	}
+
+	void TFTDisplay::DisplayTemperatureScreen()
+	{
+		this->LayoutTemperatureScreen();
+
+		noInterrupts();
+
+		this->LoadMessage(F("Coming Soon!"));
+		this->PrintMessage();
+
+		interrupts();
+	}
+
+	void TFTDisplay::DisplaySettingsScreen()
+	{
+		this->LayoutSettingsScreen();
+
+		int fm = freeMemory();
+
+		noInterrupts();
+
+		if (this->previousFreeMemory != fm || this->regionChanged)
+		{
+			this->PrintFreeMemory(fm);
+		}
+
+		interrupts();
+	}
+
+	void TFTDisplay::DisplayMessageScreen()
+	{
+		this->LayoutMessageScreen();
+
+		noInterrupts();
+
+		this->PrintMessage();
+
+		interrupts();
+	}
+
+	void TFTDisplay::PrintHumidity(BME280Data* data, uint16_t color)
+	{
+		int8_t horizontalOffset = 7;
+		tft.setFont(&bahnschrift90pt7b);
+		tft.setTextSize(1);
+		char* humidity = BME280Data::ConvertFloatToString(data->Humidity, 2, 0);
+		int16_t centeredTextXPosition = GetCenteredPosition(humidity, humidityArea.x, humidityArea.y, humidityArea.width) - horizontalOffset;
+		tft.setCursor(centeredTextXPosition, humidityArea.y + humidityArea.height - 35);
+		tft.setTextColor(color, this->BackgroundColor); // apparently this doesn't work with custom fonts?
+		tft.print(humidity);
+	}
+
+	void TFTDisplay::PrintTemperature(BME280Data* data, uint16_t color)
+	{
+		int8_t horizontalOffset = 7;
+		tft.setFont(&bahnschrift90pt7b);
+		tft.setTextSize(1);
+		char* temperature = BME280Data::ConvertFloatToString(data->Temperature, 2, 0);
+		int16_t centeredTextXPosition = GetCenteredPosition(temperature, temperatureArea.x, temperatureArea.y, temperatureArea.width) - horizontalOffset;
+		tft.setCursor(centeredTextXPosition, temperatureArea.y + temperatureArea.height - 35);
+		tft.setTextColor(color, this->BackgroundColor); // apparently this doesn't work with custom fonts?
+		tft.print(temperature);
+	}
+
+	/// <summary>Prints the amount of free memory in bytes to the screen.</summary>
+	/// <param name="freeMemory">The amount of free memory to display, in bytes.</param>
+	void TFTDisplay::PrintFreeMemory(int freeMemory)
+	{
+		char* memoryLabel = "Free SRAM: ";
+
+		noInterrupts();
+
+		tft.setFont(&calibrib12pt7b);
+		tft.setTextSize(1);
+
+		// overwrite
+		tft.setCursor(145, 160);
+		tft.setTextColor(TFTDisplay::BackgroundColor);
+		tft.print(memoryLabel);
+		tft.print(previousFreeMemory);
+
+		// print the value
+		tft.setCursor(145, 160);
+		tft.setTextColor(TFTDisplay::ReadingsTextColor);
+		tft.print(memoryLabel);
+		tft.print(freeMemory);
+
+		interrupts();
+
+		this->previousFreeMemory = freeMemory;
+	}
+
+	void TFTDisplay::PrintMessage()
+	{
+		tft.setFont(&calibrib12pt7b);
+		tft.setTextSize(1);
+
+		int centeredLocation = GetCenteredPosition(this->previousMessage, 0, 160, 480);
+		tft.setCursor(centeredLocation, 160);
+		tft.setTextColor(TFTDisplay::BackgroundColor);
+		tft.print(this->previousMessage);
+
+		centeredLocation = GetCenteredPosition(this->currentMessage, 0, 160, 480);
+		tft.setCursor(centeredLocation, 160);
+		tft.setTextColor(TFTDisplay::ReadingsTextColor);
+		tft.print(this->currentMessage);
+
+		this->previousMessage = this->currentMessage;
+	}
+
+	bool TFTDisplay::IntegerPartChanged(float first, float second)
+	{
+		/*double fractpart1, intpart1, fractpart2, intpart2;
+
+		fractpart1 = modf(first, &intpart1);
+		fractpart2 = modf(second, &intpart2);
+
+		return intpart1 != intpart2;*/
+		char* f = BME280Data::ConvertFloatToString(first, 2, 0);
+		char* s = BME280Data::ConvertFloatToString(second, 2, 0);
+
+		return strcmp(f, s) != 0;
+	}
+
+	int16_t TFTDisplay::GetCenteredPosition(char* text, int16_t x, int16_t y, int16_t areaWidth)
+	{
+		// values used for centering text in display areas (rectangles)
+		int16_t  x1, y1;
+		uint16_t textWidth, textHeight;
+
+		tft.getTextBounds(text, x, y, &x1, &y1, &textWidth, &textHeight);
+		return ((areaWidth - textWidth) / 2) + x;
+	}
+
+	int16_t TFTDisplay::GetCenteredPosition(const char* text, int16_t x, int16_t y, int16_t areaWidth)
+	{
+		// values used for centering text in display areas (rectangles)
+		int16_t  x1, y1;
+		uint16_t textWidth, textHeight;
+		tft.getTextBounds(text, x, y, &x1, &y1, &textWidth, &textHeight);
+		return ((areaWidth - textWidth) / 2) + x;
+	}
+
+	int16_t TFTDisplay::GetCenteredPosition(const __FlashStringHelper* text, int16_t x, int16_t y, int16_t areaWidth)
+	{
+		// values used for centering text in display areas (rectangles)
+		int16_t  x1, y1;
+		uint16_t textWidth, textHeight;
+		tft.getTextBounds(text, x, y, &x1, &y1, &textWidth, &textHeight);
+		return ((areaWidth - textWidth) / 2) + x;
+	}
+}
---
+++ src/Display/TFTDisplay.h
@@ -0,0 +1,159 @@
+#ifndef TFTDisplay_h
+#define TFTDisplay_h
+
+#ifdef ESP8266
+#define STMPE_CS 16
+#define TFT_CS   0
+#define TFT_DC   15
+#define SD_CS    2
+#endif
+#ifdef ESP32
+#define STMPE_CS 32
+#define TFT_CS   15
+#define TFT_DC   33
+#define SD_CS    14
+#endif
+#ifdef TEENSYDUINO
+#define TFT_DC   10
+#define TFT_CS   4
+#define STMPE_CS 3
+#define SD_CS    8
+#endif
+#ifdef ARDUINO_STM32_FEATHER
+#define TFT_DC   PB4
+#define TFT_CS   PA15
+#define STMPE_CS PC7
+#define SD_CS    PC5
+#endif
+#ifdef ARDUINO_NRF52_FEATHER /* BSP 0.6.5 and higher! */
+#define TFT_DC   11
+#define TFT_CS   31
+#define STMPE_CS 30
+#define SD_CS    27
+#endif
+#if defined(ARDUINO_MAX32620FTHR) || defined(ARDUINO_MAX32630FTHR)
+#define TFT_DC   P5_4
+#define TFT_CS   P5_3
+#define STMPE_CS P3_3
+#define SD_CS    P3_2
+#endif
+
+// Anything else!
+#if defined (__AVR_ATmega32U4__) || defined(ARDUINO_SAMD_FEATHER_M0) || defined (__AVR_ATmega328P__) || defined(ARDUINO_SAMD_ZERO) || defined(__SAMD51__) || defined(__SAM3X8E__)
+#define STMPE_CS 6
+#define TFT_CS   9
+#define TFT_DC   10
+#define SD_CS    5
+#endif
+
+// external dependencies
+#include <Arduino.h>
+#include <math.h>
+#include <SPI.h>
+#include <Wire.h>      // this is needed even tho we aren't using it
+#include <Adafruit_GFX.h>
+#include <Adafruit_HX8357.h>
+#include <Adafruit_STMPE610.h>
+#include <MemoryFree.h>
+
+// include some fonts
+#include "Fonts\bahnschrift90pt7b.h"
+#include "Fonts\calibrib12pt7b.h"
+
+#include "..\InitializationResult.h"
+#include "..\Sensors\BME280Data.h"
+#include "..\Rectangle.h"
+#include "ButtonRectangle.h"
+#include "ScreenRegion.h"
+
+using namespace Sensors;
+
+namespace Display {
+	/// <summary>Defines a class that controls the display of the monitor unit.</summary>
+	class TFTDisplay
+	{
+	public:
+		TFTDisplay();
+		InitializationResult Initialize();
+		void LoadMessage(const __FlashStringHelper* message);
+		void LoadSensorData(BME280Data data);
+		void Display();
+		void Display(ScreenRegion region);
+		void Clear();
+		ScreenRegion Touched();
+	private:
+		// a constant that defines the rotational position of the display.
+		static const uint8_t ScreenOrientation = 1;
+
+		// Interface color constants
+		static const uint16_t ReadingsTextColor = 0x5DDF;
+		static const uint16_t LayoutTextColor = HX8357_WHITE;
+		static const uint16_t ErrorTextColor = HX8357_YELLOW;
+		static const uint16_t LayoutLineColor = HX8357_WHITE;
+		static const uint16_t BackgroundColor = HX8357_BLACK;
+
+		// Display unit variables
+		Adafruit_HX8357 tft;
+		Adafruit_STMPE610 ts;
+		uint16_t height;
+		uint16_t width;
+
+		// variables used for display state management
+		bool dataChanged = false;
+		bool regionChanged = false;
+		BME280Data currentBME280Data;
+		BME280Data previousBME280Data;
+		const __FlashStringHelper* currentMessage;
+		const __FlashStringHelper* previousMessage;
+		int previousFreeMemory = 0;
+
+		// constants and variables for the touchscreen
+		bool touchscreenExists = false;
+		ScreenRegion selectedRegion = ScreenRegion::None;
+		ScreenRegion activeRegion = ScreenRegion::None;
+		// these constants are used to calculate the offsets of the touchscreen
+		static const uint16_t TSMinX = 150;
+		static const uint16_t TSMinY = 130;
+		static const uint16_t TSMaxX = 3800;
+		static const uint16_t TSMaxY = 4000;
+
+		// Define the rectangles that are used to draw the screen layout elements
+		ButtonRectangle humidityArea;
+		ButtonRectangle temperatureArea;
+		ButtonRectangle settingsButton;
+		ButtonRectangle homeButton;
+		Rectangle centeredMessageBox;
+
+		// private functions for layout and screen presentation
+		void LayoutHomeScreen();
+		void DisplayHomeScreen();
+
+		void LayoutHumidityScreen();
+		void DisplayHumidityScreen();
+		void PrintHumidity(BME280Data* data, uint16_t color);
+
+		void LayoutTemperatureScreen();
+		void DisplayTemperatureScreen();
+		void PrintTemperature(BME280Data* data, uint16_t color);
+
+		void LayoutMessageScreen();
+		void DisplayMessageScreen();
+		void PrintMessage();
+
+		void LayoutSettingsScreen();
+		void DisplaySettingsScreen();
+
+		void PrintFreeMemory(int freeMemory);
+
+		void ConfigureButtons(ScreenRegion region);
+		bool DisplayUpdatable();
+		void DisplayUpdated();
+		bool IntegerPartChanged(float first, float second);
+
+		int16_t GetCenteredPosition(char* text, int16_t x, int16_t y, int16_t areaWidth);
+		int16_t GetCenteredPosition(const char* text, int16_t x, int16_t y, int16_t areaWidth);
+		int16_t GetCenteredPosition(const __FlashStringHelper* text, int16_t x, int16_t y, int16_t areaWidth);
+	};
+}
+
+#endif
